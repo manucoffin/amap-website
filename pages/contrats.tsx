90d870143183d967d006fdab89db98653511c6bf
manucoffin <manuel.coffin@gmail.com>
2022-02-17 22:53:22 +0000

    feat(contracts): added footer to contract page

--- pages/contrats.tsx
+++ pages/contrats.tsx
@@ -1,36 +1,42 @@
 import ContractCard from "components/ContractCard";
+import { MainLayout } from "layouts";
 import { Contract, getAllContracts } from "lib/contracts";
-import { Contrats } from "lib/netlify-types";
+import { getFooter } from "lib/footer";
+import { Footer } from "lib/netlify-types";
 import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import * as React from "react";
 import { PlasmicContracts } from "../components/plasmic/amap_website/PlasmicContracts";
 
 const ContractsPage: NextPage<
   InferGetStaticPropsType<typeof getStaticProps>
-> = ({ contracts }) => {
+> = ({ contracts, footerData }) => {
   return (
-    <PlasmicContracts
-      contracts={contracts.map((contract) => (
-        <ContractCard
-          key={contract.title}
-          documentUrl={contract.document_path}
-          imageUrl={contract.image_path}
-          title={contract.title}
-          description={contract.description}
-        />
-      ))}
-    />
+    <MainLayout footerData={footerData}>
+      <PlasmicContracts
+        contracts={contracts.map((contract) => (
+          <ContractCard
+            key={contract.title}
+            documentUrl={contract.document_path}
+            imageUrl={contract.image_path}
+            title={contract.title}
+            description={contract.description}
+          />
+        ))}
+      />
+    </MainLayout>
   );
 };
 
 export const getStaticProps: GetStaticProps<{
   contracts: Contract[];
+  footerData: Footer;
 }> = () => {
   const contracts = getAllContracts();
+  const footerData = getFooter();
 
   return {
     revalidate: 1,
-    props: { contracts },
+    props: { contracts, footerData },
   };
 };
 
