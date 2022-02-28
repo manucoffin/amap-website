6858f829beff36b625c845c97fda099222b5345b
manucoffin <manuel.coffin@gmail.com>
2022-02-28 10:41:26 +0000

    try to use netlify identity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
@@ -6,11 +6,7 @@ class MyDocument extends Document {
       <Html className="scroll-smooth" lang="fr">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Aleo:wght@400;700&family=Amatic+SC:wght@400;700&family=Dosis:wght@500;800&display=swap"
             rel="stylesheet"
--- pages/admin.tsx
+++ pages/admin.tsx
@@ -2,15 +2,26 @@
 // https://www.manuelkruisz.com/blog/posts/custom-previews-nextjs-netlifycms
 import { useEffect } from 'react';
 import { Widget as IdWidget } from '@ncwidgets/id';
+// import netlifyIdentity from 'netlify-identity-widget';
+// import netlifyAuth from 'lib/netlify-auth.js';
 import config from '../public/admin/config';
 
 const AdminPage: React.FC = () => {
+  // const [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated);
+
   useEffect(() => {
     (async () => {
       const CMS = (await import('netlify-cms-app')).default;
+      // netlifyIdentity.init({
+      //   APIUrl: 'https://amap-goutte-eau.netlify.app/.netlify/identity',
+      //   locale: 'fr',
+      // });
       CMS.registerWidget(IdWidget.name);
       CMS.init({ config } as any);
     })();
+    // netlifyAuth.initialize((user) => {
+    //   setLoggedIn(!!user);
+    // });
   }, []);
   return <div />;
 };
