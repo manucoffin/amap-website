41f6ca72eaf5aa1090be75668d9ba27009822e0a
manucoffin <manuel.coffin@gmail.com>
2022-03-04 11:25:17 +0000

    refacto: removed next Optimized Images plugin

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // import withPlugins from 'next-compose-plugins';
 // import optimizedImages from 'next-optimized-images';
 const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
+// const optimizedImages = require('next-optimized-images');
 
 const nextConfig = {
   reactStrictMode: true,
@@ -15,16 +15,17 @@ const nextConfig = {
   },
 };
 
+// Not necessary when using next/image component
 // More infos about plugin configuration here:
 // https://github.com/cyrilwanner/next-optimized-images
-const nextOptimizedImagesPlugin = [
-  optimizedImages,
-  {
-    mozjpeg: { quality: 80 },
-    optipng: {
-      optimizationLevel: 3,
-    },
-  },
-];
+// const nextOptimizedImagesPlugin = [
+//   optimizedImages,
+//   {
+//     mozjpeg: { quality: 80 },
+//     optipng: {
+//       optimizationLevel: 3,
+//     },
+//   },
+// ];
 
-module.exports = withPlugins([nextOptimizedImagesPlugin], nextConfig);
+module.exports = withPlugins([], nextConfig);
