23bf44dea8990e54c9d096b727fc8761ec898b26
manucoffin <manuel.coffin@gmail.com>
2022-03-27 22:47:02 +0100

    added abla analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Aleo:wght@400;700&family=Amatic+SC:wght@400;700&display=swap"
             rel="stylesheet"
           />
+          <script async defer data-website-id="8d6a2634-f49c-413b-8f56-e8c5b65227e5" src="https://s.abla.io/abla.js"></script>
         </Head>
         <body>
           <Main />
