f0ecef9e6b6b795bfde9b7f247c0477e4c7220a2
manucoffin <manuel.coffin@gmail.com>
2022-06-06 17:02:21 +0100

    fix: removed plasmic app provider

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,5 +1,4 @@
 import '../styles/global.css';
-import { PlasmicRootProvider } from '@plasmicapp/react-web';
 import PlausibleProvider from 'next-plausible';
 import { AppProps } from 'next/app';
 import { DefaultSeo } from 'src/core/components/Seo';
@@ -10,10 +9,8 @@ config({ ssrFadeout: true });
 function App({ Component, pageProps }: AppProps) {
   return (
     <PlausibleProvider domain="amap-goutte-eau.fr">
-      <PlasmicRootProvider>
-        <DefaultSeo />
-        <Component {...pageProps} />
-      </PlasmicRootProvider>
+      <DefaultSeo />
+      <Component {...pageProps} />
     </PlausibleProvider>
   );
 }
