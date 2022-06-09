a2d0f9a33b88d4faa83aece5148f54dc0a431430
manucoffin <manuel.coffin@gmail.com>
2022-06-09 09:59:00 +0100

    added identity in _document

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
@@ -7,6 +8,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
         </Head>
         <body>
           <Main />
