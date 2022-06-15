87a59af68273bcd75ecfd799833b80840c2a8f04
manucoffin <manuel.coffin@gmail.com>
2022-06-15 10:41:38 +0200

    manual identity init

--- src/pages/admin.page.tsx
+++ src/pages/admin.page.tsx
@@ -4,36 +4,22 @@ import { useEffect } from 'react';
 import { Widget as IdWidget } from '@ncwidgets/id';
 import { config } from '../../public/admin/config.src';
 
-// import Link from 'next/link';
-// import { useContext } from 'react';
-// import { AuthContext } from '@core/contexts/AuthContext';
-
-// const NavBar = () => {
-//   const { user, login, logout } = useContext(AuthContext);
-//   return (
-//     <nav>
-//       <Link href="/">
-//         <a>Home</a>
-//       </Link>
-//       <Link href="/about">
-//         <a>About</a>
-//       </Link>
-//       <li>{!user && <button onClick={login}>Login/Signup</button>}</li>
-//       <li>{user && <button onClick={logout}>Logout</button>}</li>
-//     </nav>
-//   );
-// };
-
 const AdminPage = () => {
   useEffect(() => {
     (async () => {
       const CMS = (await import('netlify-cms-app')).default;
+      const identity = (await import('netlify-identity-widget')).default;
+
       CMS.registerWidget(IdWidget.name, IdWidget.controlComponent);
       CMS.init({ config } as any); // TODO: type properly and enable eslint no-explicit-any rule
+      identity.init({
+        locale: 'fr',
+        APIUrl: 'https://amap-goutte-eau.netlify.app/.netlify/identity',
+      });
     })();
   }, []);
 
-  return <div>{/* <NavBar /> */}</div>;
+  return <div />;
 };
 
 export default AdminPage;
