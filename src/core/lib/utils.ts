7c45de6bd4c693d6bacc0bb229f1f2dc0c96ca12
manucoffin <manuel.coffin@gmail.com>
2024-10-10 19:58:04 +0700

    auto commit

--- src/core/lib/utils.ts
+++ src/core/lib/utils.ts
@@ -1,3 +1,5 @@
 export const truncateText = (str: string, length: number): string => {
   return str.length > length ? `${str.substring(0, length)}...` : str;
 };
+
+export const isProduction = process.env.NODE_ENV === 'production';
--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,18 +3,26 @@ import { AppProps } from 'next/app';
 import { DefaultSeo } from 'src/core/components/Seo';
 import config from 'react-reveal/globals';
 import Script from 'next/script';
+import { isProduction } from '@src/core/lib';
 
 config({ ssrFadeout: true });
 
 function App({ Component, pageProps }: AppProps) {
+  const analyticsSrc = process.env.ANALYTICS_URL;
+  const websiteId = process.env.ANALYTICS_WEBSITE_ID;
+
   return (
     <>
-      <Script
+      {isProduction && (
+        <Script strategy="afterInteractive" defer src={analyticsSrc} data-website-id={websiteId} />
+      )}
+
+      {/* <Script
         strategy="afterInteractive"
         data-domain="amap-goutte-eau.fr"
         defer
         src="https://plausible.io/js/plausible.js"
-      />
+      /> */}
 
       <DefaultSeo />
       <Component {...pageProps} />
